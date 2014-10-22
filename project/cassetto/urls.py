1ed38b34fbecb1a74e506aa04bdff22322784165
joke2k <joke2k@gmail.com>
2014-10-22 16:29:20 +0200

    skip django_toolbar loading into urls

--- project/cassetto/urls.py
+++ project/cassetto/urls.py
@@ -46,7 +46,10 @@ urlpatterns = patterns('', *urls)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += patterns('',
-                            url(r'^__debug__/', include(debug_toolbar.urls)),
-                            )
+    try:
+        import debug_toolbar
+        urlpatterns += patterns('',
+                                url(r'^__debug__/', include(debug_toolbar.urls)),
+                                )
+    except ImportError:
+        pass
